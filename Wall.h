6f4dfad7f7a3adc320a9aac577dfefaca242f2e0
consularprism99 <almazanj649@gmail.com>
2026-02-12 10:01:30 -0600

    fix

--- Wall.h
+++ Wall.h
@@ -2,22 +2,34 @@
 
 class Wall{
     public:
-    int height;
-    int width;
+    float height;
+    float width;
     Vector2 position;
+    Vector2 *positionNPC;
     bool moveable = false;
     Color sqrColor;
-    Wall(int height, int width, Vector2 position, Color sqrColor){
+    
+    Wall(float height, float width, Vector2 position, Color sqrColor){
         this->height = height;
         this->width = width;
         this->sqrColor = sqrColor;
         this->position = position;
     }
-    Wall(int height, int width, Vector2 position, bool moveable, Color sqrColor){
+    Wall(float height, float width, Vector2 position, bool moveable, Color sqrColor){
         this->height = height;
         this->width = width;
         this->moveable = moveable;
         this->sqrColor = sqrColor;
         this->position = position;
     }
+    
+    Wall(float height, float width, Vector2 *positionNPC, bool moveable, Color sqrColor){
+        this->height = height;
+        this->width = width;
+        this->moveable = moveable;
+        this->sqrColor = sqrColor;
+        this->positionNPC = positionNPC;
+    }
+    
+    
 };
